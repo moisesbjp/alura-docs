43f78d071d4ce4eb228251178fb686ef0a16b64f
Moisés Brito <moisesbritojp@hotmail.com>
2025-09-30 11:43:16 -0300

    utilziando socket.io

--- src/servidor.js
+++ src/servidor.js
@@ -1,6 +1,9 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
+import { log } from "console";
 
 const app = express();
 const porta = process.env.porta || 3000;
@@ -9,4 +12,13 @@ const caminhoAtual = url.fileURLToPath(import.meta.url);
 const diretorioPublico = path.join(caminhoAtual, "../..", "public")
 app.use(express.static(diretorioPublico));
 
-app.listen(porta, () => console.log(`Servidor escutando na porta ${porta}`));
+const servidorHttp = http.createServer(app)
+
+servidorHttp.listen(porta, () => console.log(`Servidor escutando na porta ${porta}`));
+
+const io = new Server(servidorHttp);
+
+io.on("connection", () => {
+    console.log("Um cliente se conectou!");
+    
+})
