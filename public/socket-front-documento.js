13972635733dda9bfe45834f663d18838406b545
Moisés Brito <moisesbritojp@hotmail.com>
2025-09-30 16:31:05 -0300

    Salas, armazenando localmente

--- public/socket-front-documento.js
+++ public/socket-front-documento.js
@@ -2,12 +2,18 @@ import { atualizaTextoEditor } from "./documento.js";
 
 const socket = io();
 
-function emitirTextoEditor(texto){
-    socket.emit("texto_editor", texto )
+function selecionarDocumento(nome){
+    socket.emit("selecionar_documento", nome, (texto) => {
+        atualizaTextoEditor(texto)
+    })
 }
 
-socket.on("texto_editor_clientes", (texto) =>{
-    atualizaTextoEditor(texto)      
-});
+function emitirTextoEditor(dados){
+    socket.emit("texto_editor", dados)
+}
 
-export {emitirTextoEditor}
+socket.on("texto_documento", (texto) => {
+    atualizaTextoEditor(texto)
+})
+
+export {emitirTextoEditor, selecionarDocumento}
